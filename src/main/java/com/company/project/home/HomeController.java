8d15112689b6f351fa0096b64b1971979770dc23
Alexandre Gama <login+alexandregama@elo7.com>
2015-01-28 23:29:19 -0200

    First commit with all structure

---
+++ src/main/java/com/company/project/home/HomeController.java
@@ -0,0 +1,13 @@
+package com.company.project.home;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+
+@Controller
+public class HomeController {
+
+	@Get("/show")
+	public void show() {
+	}
+
+}
