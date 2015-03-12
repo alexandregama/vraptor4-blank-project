b0c95a1a0594b1b9a32d659dfa7895991c741879
Alexandre Gama <login+alexandregama@elo7.com>
2015-03-12 11:36:05 -0300

    Adds some unit tests to User

--- src/test/java/com/procurandoape/user/UserTest.java
+++ src/test/java/com/procurandoape/user/UserTest.java
@@ -1,7 +1,10 @@
 package com.procurandoape.user;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 import org.junit.Test;
@@ -16,4 +19,79 @@ public class UserTest {
 		assertEquals(28, user.getAge());
 	}
 
+	@Test
+	public void shouldReturnAFullName() throws Exception {
+		User user = new User();
+		user.setFirstName("Alexandre");
+		user.setLastName("Gama");
+
+		assertEquals("Alexandre Gama", user.getFullName());
+	}
+
+	@Test
+	public void shouldConfirmAPassword() throws Exception {
+		User user = new User();
+		user.setPassword("123456");
+		user.setPasswordConfirmation("123456");
+
+		assertTrue(user.hasValidPasswordConfirmation());
+	}
+
+	@Test
+	public void shouldNotConfirmAPassword() throws Exception {
+		User user = new User();
+		user.setPassword("654321");
+		user.setPasswordConfirmation("123456");
+
+		assertFalse(user.hasValidPasswordConfirmation());
+	}
+
+	@Test
+	public void shouldCheckIfMaleGenderIsSelected() throws Exception {
+		User user = new User();
+		user.setGender(Gender.MALE);
+
+		assertTrue(user.isMaleSelected());
+	}
+
+	@Test
+	public void shouldCheckIfFemaleGenderIsSelected() throws Exception {
+		User user = new User();
+		user.setGender(Gender.FEMALE);
+
+		assertTrue(user.isFemaleSelected());
+	}
+
+	@Test
+	public void shouldCheckIfOtherGenderIsSelected() throws Exception {
+		User user = new User();
+		user.setGender(Gender.OTHER);
+
+		assertTrue(user.isOtherSelected());
+	}
+
+	@Test
+	public void shouldCheckIfUserSelectedThatHeHasAPlace() throws Exception {
+		User user = new User();
+		user.setPlaceType(PlaceType.HAS_PLACE);
+
+		assertTrue(user.isHasPlaceSelected());
+	}
+
+	@Test
+	public void shouldCheckIfUserSelectedThatHeNeedsAPlace() throws Exception {
+		User user = new User();
+		user.setPlaceType(PlaceType.NEEDS_PLACE);
+
+		assertTrue(user.isNeedsPlaceSelected());
+	}
+
+	@Test
+	public void shouldFormatABirthdayUser() throws Exception {
+		Calendar birthday = new GregorianCalendar(1986, 05, 25);
+		User user = new User();
+		user.setBirthday(birthday);
+
+		assertEquals("25/06/1986", user.getFormattedBirthday());
+	}
 }
